9e05aac918a3cb95dc4f1e7689806384cdcac30e
RWB0104 <psj1789@naver.com>
2019-08-31 19:38:38 +0900

    repo init

---
+++ DevelopFileGenerator/MainForm.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace DevelopFileGenerator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.textBoxFolder = new System.Windows.Forms.TextBox();
+            this.buttonPath = new System.Windows.Forms.Button();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.labelCopyright = new System.Windows.Forms.Label();
+            this.buttonSubmit = new System.Windows.Forms.Button();
+            this.textBoxFile = new System.Windows.Forms.TextBox();
+            this.numericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.labelProgress = new System.Windows.Forms.Label();
+            this.labelCount = new System.Windows.Forms.Label();
+            this.buttonInfo = new System.Windows.Forms.Button();
+            this.labelPath = new System.Windows.Forms.Label();
+            this.labelFolder = new System.Windows.Forms.Label();
+            this.labelFile = new System.Windows.Forms.Label();
+            this.labelFormat = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(16, 23);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(348, 23);
+            this.progressBar.TabIndex = 0;
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Location = new System.Drawing.Point(76, 107);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.Size = new System.Drawing.Size(248, 21);
+            this.textBoxPath.TabIndex = 2;
+            this.textBoxPath.TextChanged += new System.EventHandler(this.TextBoxPath_TextChanged);
+            // 
+            // textBoxFolder
+            // 
+            this.textBoxFolder.Location = new System.Drawing.Point(76, 139);
+            this.textBoxFolder.Name = "textBoxFolder";
+            this.textBoxFolder.Size = new System.Drawing.Size(248, 21);
+            this.textBoxFolder.TabIndex = 4;
+            this.textBoxFolder.TextChanged += new System.EventHandler(this.TextBoxFolder_TextChanged);
+            // 
+            // buttonPath
+            // 
+            this.buttonPath.Location = new System.Drawing.Point(330, 106);
+            this.buttonPath.Name = "buttonPath";
+            this.buttonPath.Size = new System.Drawing.Size(34, 23);
+            this.buttonPath.TabIndex = 3;
+            this.buttonPath.Text = "Set";
+            this.buttonPath.UseVisualStyleBackColor = true;
+            this.buttonPath.Click += new System.EventHandler(this.ButtonPath_Click);
+            // 
+            // labelCopyright
+            // 
+            this.labelCopyright.AutoSize = true;
+            this.labelCopyright.Location = new System.Drawing.Point(74, 273);
+            this.labelCopyright.Name = "labelCopyright";
+            this.labelCopyright.Size = new System.Drawing.Size(240, 12);
+            this.labelCopyright.TabIndex = 4;
+            this.labelCopyright.Text = "Copyright (c) RWB All Copyright Reserve.";
+            // 
+            // buttonSubmit
+            // 
+            this.buttonSubmit.Location = new System.Drawing.Point(289, 60);
+            this.buttonSubmit.Name = "buttonSubmit";
+            this.buttonSubmit.Size = new System.Drawing.Size(75, 23);
+            this.buttonSubmit.TabIndex = 7;
+            this.buttonSubmit.Text = "Submit";
+            this.buttonSubmit.UseVisualStyleBackColor = true;
+            this.buttonSubmit.Click += new System.EventHandler(this.ButtonSubmit_Click);
+            // 
+            // textBoxFile
+            // 
+            this.textBoxFile.Location = new System.Drawing.Point(76, 171);
+            this.textBoxFile.Name = "textBoxFile";
+            this.textBoxFile.Size = new System.Drawing.Size(248, 21);
+            this.textBoxFile.TabIndex = 5;
+            this.textBoxFile.TextChanged += new System.EventHandler(this.TextBoxFile_TextChanged);
+            // 
+            // numericUpDown
+            // 
+            this.numericUpDown.Location = new System.Drawing.Point(61, 63);
+            this.numericUpDown.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.numericUpDown.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown.Name = "numericUpDown";
+            this.numericUpDown.Size = new System.Drawing.Size(60, 21);
+            this.numericUpDown.TabIndex = 1;
+            this.numericUpDown.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown.ValueChanged += new System.EventHandler(this.NumericUpDown_ValueChanged);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(206, 60);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(75, 23);
+            this.buttonReset.TabIndex = 6;
+            this.buttonReset.Text = "Reset";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.ButtonReset_Click);
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.WorkerReportsProgress = true;
+            this.backgroundWorker.WorkerSupportsCancellation = true;
+            this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BackgroundWorker_DoWork);
+            this.backgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BackgroundWorker_RunWorkerCompleted);
+            // 
+            // labelProgress
+            // 
+            this.labelProgress.AutoSize = true;
+            this.labelProgress.Location = new System.Drawing.Point(25, 7);
+            this.labelProgress.Name = "labelProgress";
+            this.labelProgress.Size = new System.Drawing.Size(0, 12);
+            this.labelProgress.TabIndex = 9;
+            // 
+            // labelCount
+            // 
+            this.labelCount.Location = new System.Drawing.Point(254, 7);
+            this.labelCount.Name = "labelCount";
+            this.labelCount.Size = new System.Drawing.Size(99, 13);
+            this.labelCount.TabIndex = 10;
+            this.labelCount.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // buttonInfo
+            // 
+            this.buttonInfo.Location = new System.Drawing.Point(16, 61);
+            this.buttonInfo.Name = "buttonInfo";
+            this.buttonInfo.Size = new System.Drawing.Size(36, 23);
+            this.buttonInfo.TabIndex = 11;
+            this.buttonInfo.Text = "Info";
+            this.buttonInfo.UseVisualStyleBackColor = true;
+            this.buttonInfo.Click += new System.EventHandler(this.ButtonInfo_Click);
+            // 
+            // labelPath
+            // 
+            this.labelPath.Location = new System.Drawing.Point(12, 107);
+            this.labelPath.Name = "labelPath";
+            this.labelPath.Size = new System.Drawing.Size(58, 21);
+            this.labelPath.TabIndex = 12;
+            this.labelPath.Text = "경로";
+            this.labelPath.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // labelFolder
+            // 
+            this.labelFolder.Location = new System.Drawing.Point(12, 139);
+            this.labelFolder.Name = "labelFolder";
+            this.labelFolder.Size = new System.Drawing.Size(58, 21);
+            this.labelFolder.TabIndex = 13;
+            this.labelFolder.Text = "폴더 이름";
+            this.labelFolder.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // labelFile
+            // 
+            this.labelFile.Location = new System.Drawing.Point(12, 171);
+            this.labelFile.Name = "labelFile";
+            this.labelFile.Size = new System.Drawing.Size(58, 21);
+            this.labelFile.TabIndex = 14;
+            this.labelFile.Text = "파일 형식";
+            this.labelFile.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // labelFormat
+            // 
+            this.labelFormat.BackColor = System.Drawing.Color.White;
+            this.labelFormat.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelFormat.Location = new System.Drawing.Point(14, 212);
+            this.labelFormat.Name = "labelFormat";
+            this.labelFormat.Size = new System.Drawing.Size(350, 48);
+            this.labelFormat.TabIndex = 16;
+            this.labelFormat.Text = "Path Formatter";
+            this.labelFormat.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(378, 295);
+            this.Controls.Add(this.labelFormat);
+            this.Controls.Add(this.labelFile);
+            this.Controls.Add(this.labelFolder);
+            this.Controls.Add(this.labelPath);
+            this.Controls.Add(this.buttonInfo);
+            this.Controls.Add(this.labelCount);
+            this.Controls.Add(this.labelProgress);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.numericUpDown);
+            this.Controls.Add(this.textBoxFile);
+            this.Controls.Add(this.buttonSubmit);
+            this.Controls.Add(this.labelCopyright);
+            this.Controls.Add(this.buttonPath);
+            this.Controls.Add(this.textBoxFolder);
+            this.Controls.Add(this.textBoxPath);
+            this.Controls.Add(this.progressBar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "DevelopFileGenerator";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.TextBox textBoxPath;
+        private System.Windows.Forms.TextBox textBoxFolder;
+        private System.Windows.Forms.Button buttonPath;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.Label labelCopyright;
+        private System.Windows.Forms.Button buttonSubmit;
+        private System.Windows.Forms.TextBox textBoxFile;
+        private System.Windows.Forms.NumericUpDown numericUpDown;
+        private System.Windows.Forms.Button buttonReset;
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+        private System.Windows.Forms.Label labelProgress;
+        private System.Windows.Forms.Label labelCount;
+        private System.Windows.Forms.Button buttonInfo;
+        private System.Windows.Forms.Label labelPath;
+        private System.Windows.Forms.Label labelFolder;
+        private System.Windows.Forms.Label labelFile;
+        private System.Windows.Forms.Label labelFormat;
+    }
+}
+
---
+++ DevelopFileGenerator/MainForm.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace DevelopFileGenerator
+{
+    public partial class MainForm : Form
+    {
+        private string fullPath = null;
+        private string fullFile = null;
+
+        private string path = null;
+        private string folder = null;
+        private string file = null;
+        private decimal maxCount = 1;
+
+        private int count = 1;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        // 프로그램 로딩 시 마지막으로 입력된 값 자동입력
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            textBoxPath.Text = Properties.Settings.Default.path;
+            textBoxFolder.Text = Properties.Settings.Default.folder;
+            textBoxFile.Text = Properties.Settings.Default.file;
+            numericUpDown.Value = Properties.Settings.Default.maxCount;
+
+            PathFormat();
+        }
+
+        // 정보 버튼
+        private void ButtonInfo_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("설정파일 위치 : " + ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.PerUserRoamingAndLocal).FilePath + "\n\n" + "Icon : " + "https://www.iconfinder.com/korawan_m", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        // 경로 지정
+        private void ButtonPath_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBoxPath.Text = folderBrowserDialog.SelectedPath;
+            }
+        }
+
+        // 경로 직접 입력
+        private void TextBoxPath_TextChanged(object sender, EventArgs e)
+        {
+            path = textBoxPath.Text;
+            Properties.Settings.Default.path = path;
+
+            PathFormat();
+        }
+
+        // 폴더 이름 지정
+        private void TextBoxFolder_TextChanged(object sender, EventArgs e)
+        {
+            folder = textBoxFolder.Text;
+            Properties.Settings.Default.folder = folder;
+
+            PathFormat();
+        }
+
+        // 파일 이름 지정
+        private void TextBoxFile_TextChanged(object sender, EventArgs e)
+        {
+            file = textBoxFile.Text;
+            Properties.Settings.Default.file = file;
+
+            PathFormat();
+        }
+
+        // 파일 최대 갯수 지정
+        private void NumericUpDown_ValueChanged(object sender, EventArgs e)
+        {
+            maxCount = numericUpDown.Value;
+            Properties.Settings.Default.maxCount = maxCount;
+        }
+
+        // 동작 버튼
+        private void ButtonSubmit_Click(object sender, EventArgs e)
+        {
+            // 폴더 경로가 지정되지 않을 경우
+            if (textBoxPath.Text.Equals("") || textBoxPath.Text == null)
+            {
+                ActiveControl = textBoxPath;
+                MessageBox.Show("폴더 경로는 필수로 입력해야합니다.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            // 파일 이름이 업력되지 않을 경우
+            else if (textBoxFile.Text.Equals("") || textBoxFile.Text == null)
+            {
+                ActiveControl = textBoxFile;
+                MessageBox.Show("파일 이름은 필수로 입력해야합니다.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            // 모든 필수 항목이 입력될 경우
+            else
+            {
+                backgroundWorker.RunWorkerAsync();
+            }
+        }
+
+        // 종료 처리
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.Save();
+        }
+
+        // 프로그램 동작
+        private void BackgroundWorker_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            count = 0;
+
+            progressBar.Maximum = Convert.ToInt32(maxCount) + 3;
+            labelCount.Text = "총 " + maxCount + "개";
+
+            // 동작 시도
+            try
+            {
+                buttonSubmit.Enabled = false;
+
+                // 폴더 이름이 지정되지 않을 경우
+                if (folder == null)
+                {
+                    fullPath = path;
+                }
+
+                // 폴더 이름이 지정될 경우 전체경로에 폴머 이름 추가
+                else
+                {
+                    fullPath = path + "\\" + folder;
+                }
+
+                progressBar.Value = ++count;
+                labelProgress.Text = "경로 지정";
+
+                // 생성한 파일 경로 형식 지정
+                fullFile = fullPath + "\\" + file;
+
+                progressBar.Value = ++count;
+                labelProgress.Text = "파일 이름 형식 지정";
+
+                DirectoryInfo directory = new DirectoryInfo(fullPath);
+                StreamWriter writer = null;
+
+                // 전체경로가 존제하지 않을 경우 생성
+                if (!directory.Exists)
+                {
+                    progressBar.Value = ++count;
+                    labelProgress.Text = "경로 생성";
+
+                    directory.Create();
+                }
+
+                // maxCount만큼 파일 생성
+                for (int i = 1; i <= maxCount; i++)
+                {
+                    progressBar.Value = ++count;
+                    labelProgress.Text = i + "번 째 파일 생성";
+
+                    // *을 i로 치환
+                    string temp = fullFile.Replace("*", i.ToString());
+
+                    writer = File.CreateText(temp);
+                    writer.Close();
+                }
+
+                progressBar.Value = progressBar.Maximum;
+                labelProgress.Text = "완료";
+            }
+            
+            // 오류 처리
+            catch (Exception ex)
+            {
+                labelProgress.Text = "오류 발생";
+
+                MessageBox.Show("오류 발생\n\n" + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                e.Cancel = true;
+            }
+        }
+
+        // 동작 완료
+        private void BackgroundWorker_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        {
+            // 오류로 인한 동작 취소가 아닐 경우
+            if (!e.Cancelled)
+            {
+                if (MessageBox.Show("작업 완료\n폴더를 여시겠습니까?", "Done", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
+                {
+                    Process.Start(fullPath);
+                }
+
+                progressBar.Value = 0;
+                labelProgress.Text = "";
+                labelCount.Text = "";
+            }
+
+            buttonSubmit.Enabled = true;
+        }
+
+        // 입력값 리셋
+        private void ButtonReset_Click(object sender, EventArgs e)
+        {
+            textBoxPath.Text = "";
+            textBoxFolder.Text = "";
+            textBoxFile.Text = "";
+            numericUpDown.Value = numericUpDown.Minimum;
+        }
+
+        // 경로 Formatter
+        private void PathFormat()
+        {
+            if ((path == "" || path == null) && (folder == "" || folder == null) && (file == "" || file == null))
+            {
+                labelFormat.Text = "Path Formatter";
+            }
+
+            else if ((path != "" && path != null) && (folder != "" && folder != null) && (file != "" && file != null))
+            {
+                labelFormat.Text = path + "\\" + folder + "\\" + file;
+            }
+
+            else if ((path != "" && path != null) && (folder == "" || folder == null) && (file != "" && file != null))
+            {
+                labelFormat.Text = path + "\\" + file;
+            }
+
+            else if ((path == "" || path == null) || (file == "" || file == null))
+            {
+                labelFormat.Text = "구문 오류";
+            }
+        }
+    }
+}
